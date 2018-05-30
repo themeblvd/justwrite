ded09e54d876e277c49c8d2332197c592b0d2a8c
Jason Bobich <jasonbobich@gmail.com>
2018-05-30 15:18:07 -0600

    Use configuration file for re-usable parameters.

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    errorShakeDuration: 250
+    loginEndpoint: 'wp-json/jwt-auth/v1/token',
+    verifyEndpoint: 'wp-json/jwt-auth/v1/token/validate',
+    usersEndpoint: 'wp-json/wp/v2/users',
+    errorShakeDuration: 250,
+    fadeAppDuration: 500,
+    toggleMobileMenuDuration: 500
 };
