48c9d34ebf976736e7bd36a25e9c5722063c0cc4
Jason Bobich <jasonbobich@gmail.com>
2018-06-02 17:02:19 -0600

    Allow Loader component to take a class name, so it can be styled for different places.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import classNames from 'classnames';
 
-const Loading = () => {
+/**
+ * Loader Component
+ *
+ * While styled for different locations, this
+ * is a universal loader component for the
+ * application.
+ *
+ * @param  {Object}    props           Component props.
+ * @param  {String}    props.className CSS classes.
+ * @return {Component}
+ */
+const Loading = props => {
+    const classes = classNames('loader', props.className);
+
     return (
-        <div className="loader">
+        <div className={classes}>
             <span />
         </div>
     );
